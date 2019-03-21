f0e4cabf66988f2bf976530ac02ef04edcb4c4d9
Jeremie Carlson <jeremie.carlson@instrument.com>
2019-03-21 14:59:36 -0700

    cashier excecise

--- exercises/cashier/src/cashier.ts
+++ exercises/cashier/src/cashier.ts
@@ -1,3 +1,49 @@
-// export function cashier() {
-//   return {};
-// }
+export interface CartItem {
+  name: string;
+  price: number;
+  qty?: number;
+}
+
+interface Cart {
+  length: number;
+  total: number;
+  items: CartItem[];
+  addItem: (item: CartItem) => Cart;
+  add: (
+    name: string,
+    price: number,
+    qty?: number,
+  ) => Cart;
+}
+
+export function cashier(): Cart {
+  return {
+    items: [],
+    get length() {
+      return this.items.length;
+    },
+    get total() {
+      return this.items.reduce((sum, next) => {
+        return sum + next.price;
+      }, 0)
+    },
+    addItem: function(item: CartItem) {
+      if (item.qty) {
+        for (let index = 0; index < item.qty; index++) {
+          this.items.push(item);
+        }
+      } else {
+        this.items.push(item);
+      }
+      return this;
+    },
+    add: function(name, price, qty){
+      this.addItem({
+        name,
+        price,
+        qty,
+      });
+      return this;
+    },
+  };
+}
